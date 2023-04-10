14d373a842eb696855551d1161225b74c2ee0e84
JvThunder <joshuaadrianc@gmail.com>
2023-04-11 05:34:47 +0800

    final

--- ParkWhere/backend/ParkApp/views.py
+++ ParkWhere/backend/ParkApp/views.py
@@ -61,7 +61,7 @@ class FavoriteView(APIView):
             request.user.favorite.add(carpark)
             return Response(status=200, data={"message":"Carpark added to favorites"})
         except:
-            return Response(status=400, data={"error":"Carpark not found, include id and lot type in request body. E.g. {'carpark_id': 'A0046', 'lot_type': 'C'}"})
+            return Response(status=404, data={"error":"Carpark not found, include id and lot type in request body. E.g. {'carpark_id': 'A0046', 'lot_type': 'C'}"})
 
     def delete(self, request):
         carpark_id = request.data.get('carpark_id')
@@ -71,7 +71,7 @@ class FavoriteView(APIView):
             request.user.favorite.remove(carpark)
             return Response(status=200, data={"message":"Carpark removed from favorites"})
         except:
-            return Response(status=400, data={"error":"Carpark not found, include id and lot type in request body. E.g. {'carpark_id': 'A0046', 'lot_type': 'C'}"})
+            return Response(status=404, data={"error":"Carpark not found, include id and lot type in request body. E.g. {'carpark_id': 'A0046', 'lot_type': 'C'}"})
 
 class BlacklistView(APIView):
     authentication_classes = [TokenAuthentication]
